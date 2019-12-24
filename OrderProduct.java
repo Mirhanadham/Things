649546ef34e39f378532680a7a17cd2ecf245a71
Meter Wa Nos <hanyhager120@gmail.com>
2019-12-24 18:51:33 +0200

    add orderproduct class

---
+++ OrderProduct.java
@@ -0,0 +1,24 @@
+package project;
+
+public class OrderProduct {
+
+	public int StoreProductId;
+	public int Quantity;
+	
+	
+	public int getStoreProductId() {
+		return StoreProductId;
+	}
+	public void setStoreProductId(int storeProductId) {
+		StoreProductId = storeProductId;
+	}
+	public int getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+	
+	
+	
+}
